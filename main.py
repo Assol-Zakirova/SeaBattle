90b866fb2f5adfe2a5a890b19f2dce5a4b679d81
Assol <zakirovaassol@gmail.com>
2025-12-29 16:00:36 +0600

    second commit

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 field = [['*'] * 7 for i in range(7)]
-for i in field:
-    print(*i)
-print()
 mixed_right_coordinates = [(1, 1), (1, 2), (1, 3), (4, 0), (5, 0), (3, 4), (3, 5), (0, 6), (5, 3), (6, 6), (0, 0)]
 right_coordinates = [[(1, 1), (1, 2), (1, 3)], [(4, 0), (5, 0)], [(3, 4), (3, 5)], [(0, 6)], [(5, 3)], [(6, 6)], [(0, 0)]]
+all_guessed_coordinates = []
 right_guessed_coordinates = []
-class Checking:
+class Game:
     def intro(self):
-        users_coordinate = input('Enter the coordinates:\n')
-        users_coordinate = [int(n) for n in users_coordinate.split(',')]
-        a, b = users_coordinate
-        users_coordinate = (a, b)
-        Checking().check_1(users_coordinate)
-    def check_1(self, users_coordinate):
+        print('  1 2 3 4 5 6 7')
+        rows = 'ABCDEFG'
+        for i in range(7):
+            print(rows[i], *field[i])
         while len(mixed_right_coordinates) != 0:
+            try:
+                users_coordinate = input('Enter the coordinates(from 1 to 7): like A, 1\n')
+                users_coordinate = users_coordinate.split(',')
+                a, b = users_coordinate
+                b = int(b)
+                d = {1 : 0, 2 : 1, 3 : 2, 4 : 3, 5 : 4, 6 : 5, 7 : 6}
+                d2 = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6}
+                users_coordinate = (d2[a], d[b])
+                Game().check_1(users_coordinate)
+            except:
+                Game().intro()
+    def check_1(self, users_coordinate):
+        if users_coordinate not in all_guessed_coordinates:
             if users_coordinate in mixed_right_coordinates:
                 a, b = users_coordinate
                 field[a][b] = 'H'
                 mixed_right_coordinates.remove(users_coordinate)
                 right_guessed_coordinates.append(users_coordinate)
+                all_guessed_coordinates.append(users_coordinate)
             else:
                 a, b = users_coordinate
                 field[a][b] = 'M'
-            for i in field:
-                print(*i)
-            Checking().check_2()
+                all_guessed_coordinates.append(users_coordinate)
+            Game().check_2()
+        else:
+            print('You have already entered these coordinates, please try again')
+            Game().intro()
     def check_2(self):
         for i in right_coordinates:
             l = len(i)
@@ -37,11 +49,8 @@ class Checking:
                     if counter == l:
                         for k, h in ship_for_sunk:
                             field[k][h] = 'S'
-        for p in field:
-            print(*p)
-        Checking().intro()
-y = Checking()
-y.intro()
+        Game().intro()
+Game().intro()
 
 
 
