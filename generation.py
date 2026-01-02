8751dbde7db7e46505ca50c558c3818927e4bdc1
Assol-Zakirova <zakirovaassol@gmail.com>
2026-01-03 00:46:30 +0600

    updated version

--- generation.py
+++ generation.py
@@ -1,224 +1,192 @@
 import random
-import sys
-
-sys.setrecursionlimit(5000)
-big_ship_coordinates = []
-middle_ship_coordinates = []
-little_ship_coordinates = []
-occupied_cells = []
-max_counter = 0
-c = 0
-max_counter_2 = 0
-
-
-class Calling:
-    def big_ship_call(self):
-        global occupied_cells
-        global big_ship_coordinates
-        global middle_ship_coordinates
-        global little_ship_coordinates
-        little_ship_coordinates = []
-        middle_ship_coordinates = []
-        occupied_cells = []
-        big_ship_coordinates = []
-        BigShip().generation()
-
-    def middle_ship_call(self):
-        MiddleShip().generation()
-
-    def little_ship_call(self):
-        LittleShip().generation()
-
+mixed_right_coordinates = []
+right_coordinates = []
+def adding_coordinates(var_b_s_coordinates, var_m_s_coordinates, var_l_s_coordinates):
+    global mixed_right_coordinates
+    global right_coordinates
+    for i in var_b_s_coordinates:
+        mixed_right_coordinates.append(i)
+    for j in var_m_s_coordinates:
+        mixed_right_coordinates.append(j)
+    for k in var_l_s_coordinates:
+        mixed_right_coordinates.append(k)
+    right_coordinates = [
+        var_b_s_coordinates,
+        var_m_s_coordinates[:2],
+        var_m_s_coordinates[2:],
+        [var_l_s_coordinates[0]],
+        [var_l_s_coordinates[1]],
+        [var_l_s_coordinates[2]],
+    ]
+    return mixed_right_coordinates, right_coordinates
 
 class MiddleShip:
-    def generation(self):
-        global occupied_cells
-        global middle_ship_coordinates
-        global max_counter
-        while max_counter != 400:
-            if len(middle_ship_coordinates) != 4:
-                max_counter += 1
+    def __init__(self):
+        self.middle_ship_coordinates = []
+    def generation(self, var_oc_cells):
+        while len(self.middle_ship_coordinates) != 4:
+            while True:
                 a = random.randint(1, 5)
                 b = random.randint(1, 5)
-                if (a, b) not in occupied_cells:
+                if (a, b) not in var_oc_cells:
                     direction = random.choice(['vertical', 'horizontal'])
                     if direction == 'vertical':
                         next_direction = random.choice(['up', 'down'])
                         if next_direction == 'up':
-                            if (a + 1, b) not in occupied_cells:
-                                middle_ship_coordinates.append((a, b))
+                            if (a + 1, b) not in var_oc_cells:
+                                self.middle_ship_coordinates.append((a, b))
                                 a, b = a + 1, b
-                                middle_ship_coordinates.append((a, b))
-                                for i in middle_ship_coordinates:
-                                    occupied_cells.append(i)
+                                self.middle_ship_coordinates.append((a, b))
+                                for i in self.middle_ship_coordinates:
+                                    var_oc_cells .append(i)
                                     a, b = i
-                                    occupied_cells.append((a - 1, b))
-                                    occupied_cells.append((a, b - 1))
-                                    occupied_cells.append((a + 1, b))
-                                    occupied_cells.append((a, b + 1))
-                                    occupied_cells.append((a + 1, b + 1))
-                                    occupied_cells.append((a - 1, b - 1))
-                                    occupied_cells.append((a - 1, b + 1))
-                                    occupied_cells.append((a + 1, b - 1))
-
+                                    var_oc_cells.append((a - 1, b))
+                                    var_oc_cells.append((a, b - 1))
+                                    var_oc_cells.append((a + 1, b))
+                                    var_oc_cells.append((a, b + 1))
+                                    var_oc_cells.append((a + 1, b + 1))
+                                    var_oc_cells.append((a - 1, b - 1))
+                                    var_oc_cells.append((a - 1, b + 1))
+                                    var_oc_cells .append((a + 1, b - 1))
+                                break
                             else:
-                                Calling().middle_ship_call()
+                                continue
                         elif next_direction == 'down':
-                            if (a - 1, b) not in occupied_cells:
-                                middle_ship_coordinates.append((a, b))
+                            if (a - 1, b) not in var_oc_cells:
+                                self.middle_ship_coordinates.append((a, b))
                                 a, b = a - 1, b
-                                middle_ship_coordinates.append((a, b))
-                                for i in middle_ship_coordinates:
-                                    occupied_cells.append(i)
+                                self.middle_ship_coordinates.append((a, b))
+                                for i in self.middle_ship_coordinates:
+                                    var_oc_cells .append(i)
                                     a, b = i
-                                    occupied_cells.append((a - 1, b))
-                                    occupied_cells.append((a, b - 1))
-                                    occupied_cells.append((a + 1, b))
-                                    occupied_cells.append((a, b + 1))
-                                    occupied_cells.append((a + 1, b + 1))
-                                    occupied_cells.append((a - 1, b - 1))
-                                    occupied_cells.append((a - 1, b + 1))
-                                    occupied_cells.append((a + 1, b - 1))
-
+                                    var_oc_cells.append((a - 1, b))
+                                    var_oc_cells.append((a, b - 1))
+                                    var_oc_cells.append((a + 1, b))
+                                    var_oc_cells.append((a, b + 1))
+                                    var_oc_cells.append((a + 1, b + 1))
+                                    var_oc_cells.append((a - 1, b - 1))
+                                    var_oc_cells.append((a - 1, b + 1))
+                                    var_oc_cells .append((a + 1, b - 1))
+                                break
                             else:
-                                Calling().middle_ship_call()
+                                continue
                     elif direction == 'horizontal':
                         next_direction = random.choice(['left', 'right'])
                         if next_direction == 'left':
-                            if (a, b - 1) not in occupied_cells:
-                                middle_ship_coordinates.append((a, b))
+                            if (a, b - 1) not in var_oc_cells:
+                                self.middle_ship_coordinates.append((a, b))
                                 a, b = a, b - 1
-                                middle_ship_coordinates.append((a, b))
-                                for i in middle_ship_coordinates:
-                                    occupied_cells.append(i)
+                                self.middle_ship_coordinates.append((a, b))
+                                for i in self.middle_ship_coordinates:
+                                    var_oc_cells .append(i)
                                     a, b = i
-                                    occupied_cells.append((a - 1, b))
-                                    occupied_cells.append((a, b - 1))
-                                    occupied_cells.append((a + 1, b))
-                                    occupied_cells.append((a, b + 1))
-                                    occupied_cells.append((a + 1, b + 1))
-                                    occupied_cells.append((a - 1, b - 1))
-                                    occupied_cells.append((a - 1, b + 1))
-                                    occupied_cells.append((a + 1, b - 1))
-
+                                    var_oc_cells.append((a - 1, b))
+                                    var_oc_cells.append((a, b - 1))
+                                    var_oc_cells.append((a + 1, b))
+                                    var_oc_cells.append((a, b + 1))
+                                    var_oc_cells.append((a + 1, b + 1))
+                                    var_oc_cells.append((a - 1, b - 1))
+                                    var_oc_cells.append((a - 1, b + 1))
+                                    var_oc_cells .append((a + 1, b - 1))
+                                break
                             else:
-                                Calling().middle_ship_call()
+                                continue
                         elif next_direction == 'right':
-                            if (a, b + 1) not in occupied_cells:
-                                middle_ship_coordinates.append((a, b))
+                            if (a, b + 1) not in var_oc_cells:
+                                self.middle_ship_coordinates.append((a, b))
                                 a, b = a, b + 1
-                                middle_ship_coordinates.append((a, b))
-                                for i in middle_ship_coordinates:
-                                    occupied_cells.append(i)
+                                self.middle_ship_coordinates.append((a, b))
+                                for i in self.middle_ship_coordinates:
+                                    var_oc_cells .append(i)
                                     a, b = i
-                                    occupied_cells.append((a - 1, b))
-                                    occupied_cells.append((a, b - 1))
-                                    occupied_cells.append((a + 1, b))
-                                    occupied_cells.append((a, b + 1))
-                                    occupied_cells.append((a + 1, b + 1))
-                                    occupied_cells.append((a - 1, b - 1))
-                                    occupied_cells.append((a - 1, b + 1))
-                                    occupied_cells.append((a + 1, b - 1))
-
+                                    var_oc_cells.append((a - 1, b))
+                                    var_oc_cells.append((a, b - 1))
+                                    var_oc_cells.append((a + 1, b))
+                                    var_oc_cells.append((a, b + 1))
+                                    var_oc_cells.append((a + 1, b + 1))
+                                    var_oc_cells.append((a - 1, b - 1))
+                                    var_oc_cells.append((a - 1, b + 1))
+                                    var_oc_cells.append((a + 1, b - 1))
+                                break
                             else:
-                                Calling().middle_ship_call()
+                                continue
                 else:
-                    Calling().middle_ship_call()
-            else:
-                Calling().little_ship_call()
-                break
-        else:
-            occupied_cells = []
-            max_counter = 0
-            BigShip().generation()
-
+                    continue
+        return var_oc_cells, self.middle_ship_coordinates
+class LittleShip:
+    def __init__(self):
+        self.little_ship_coordinates = []
+    def generation(self, var_oc_cells):
+        while len(self.little_ship_coordinates) != 3:
+            while True:
+                a = random.randint(0, 6)
+                b = random.randint(0, 6)
+                if (a, b) not in var_oc_cells:
+                    self.little_ship_coordinates.append((a, b))
+                    var_oc_cells.append((a, b))
+                    var_oc_cells.append((a - 1, b))
+                    var_oc_cells.append((a, b - 1))
+                    var_oc_cells.append((a + 1, b))
+                    var_oc_cells.append((a, b + 1))
+                    var_oc_cells.append((a + 1, b + 1))
+                    var_oc_cells.append((a - 1, b - 1))
+                    var_oc_cells.append((a - 1, b + 1))
+                    var_oc_cells.append((a + 1, b - 1))
+                    break
+                else:
+                    continue
+        return var_oc_cells, self.little_ship_coordinates
 
 class BigShip:
+    def __init__(self):
+        self.big_ship_coordinates = []
+        self.occupied_cells = []
     def generation(self):
-        global big_ship_coordinates
-        global occupied_cells
-        big_ship_coordinates = []
-        occupied_cells = []
         a = random.randint(2, 4)
         b = random.randint(2, 4)
-        big_ship_coordinates.append((a, b))
+        self.big_ship_coordinates.append((a, b))
         direction = random.choice(['vertical', 'horizontal'])
         if direction == 'vertical':
             next_direction = random.choice(['up', 'down'])
             if next_direction == 'up':
                 a, b = a + 1, b
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
                 a, b = a + 1, b
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
             elif next_direction == 'down':
                 a, b = a - 1, b
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
                 a, b = a - 1, b
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
         elif direction == 'horizontal':
             next_direction = random.choice(['left', 'right'])
             if next_direction == 'left':
                 a, b = a, b - 1
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
                 a, b = a, b - 1
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
             elif next_direction == 'right':
                 a, b = a, b + 1
-                big_ship_coordinates.append((a, b))
+                self.big_ship_coordinates.append((a, b))
                 a, b = a, b + 1
-                big_ship_coordinates.append((a, b))
-        for i in big_ship_coordinates:
-            occupied_cells.append(i)
+                self.big_ship_coordinates.append((a, b))
+        for i in self.big_ship_coordinates:
+            self.occupied_cells.append(i)
             a, b = i
-            occupied_cells.append((a - 1, b))
-            occupied_cells.append((a, b - 1))
-            occupied_cells.append((a + 1, b))
-            occupied_cells.append((a, b + 1))
-            occupied_cells.append((a + 1, b + 1))
-            occupied_cells.append((a - 1, b - 1))
-            occupied_cells.append((a - 1, b + 1))
-            occupied_cells.append((a + 1, b - 1))
-        Calling().middle_ship_call()
+            self.occupied_cells.append((a - 1, b))
+            self.occupied_cells.append((a, b - 1))
+            self.occupied_cells.append((a + 1, b))
+            self.occupied_cells.append((a, b + 1))
+            self.occupied_cells.append((a + 1, b + 1))
+            self.occupied_cells.append((a - 1, b - 1))
+            self.occupied_cells.append((a - 1, b + 1))
+            self.occupied_cells.append((a + 1, b - 1))
+        var_oc_cells = self.occupied_cells
+        var_b_s_coordinates = self.big_ship_coordinates
+        var_oc_cells, var_m_s_coordinates = MiddleShip().generation(var_oc_cells)
+        var_oc_cells, var_l_s_coordinates = LittleShip().generation(var_oc_cells)
+        adding_coordinates(var_b_s_coordinates, var_m_s_coordinates, var_l_s_coordinates)
 
+        
 
-class LittleShip:
-    def generation(self):
-        global occupied_cells
-        global little_ship_coordinates
-        global max_counter_2
-        global max_counter
-        global big_ship_coordinates
-        global middle_ship_coordinates
-        global c
-        while max_counter_2 != 500:
-            if len(little_ship_coordinates) != 3:
-                max_counter_2 += 1
-                a = random.randint(0, 6)
-                b = random.randint(0, 6)
-                if (a, b) not in occupied_cells:
-                    little_ship_coordinates.append((a, b))
-                    occupied_cells.append((a, b))
-                    occupied_cells.append((a - 1, b))
-                    occupied_cells.append((a, b - 1))
-                    occupied_cells.append((a + 1, b))
-                    occupied_cells.append((a, b + 1))
-                    occupied_cells.append((a + 1, b + 1))
-                    occupied_cells.append((a - 1, b - 1))
-                    occupied_cells.append((a - 1, b + 1))
-                    occupied_cells.append((a + 1, b - 1))
-                    c += 1
-                else:
-                    Calling().little_ship_call()
-                    break
-            else:
-                break
-        else:
-            max_counter = 0
-            max_counter_2 = 0
-            occupied_cells = []
-            c = 0
-            little_ship_coordinates = []
-            big_ship_coordinates = []
-            middle_ship_coordinates = []
-            Calling().big_ship_call()
 
